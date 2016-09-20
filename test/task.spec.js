85e0216e25d06fecc49b9e1b49e0c6244275cb6e
Tristan Koch <tristan@tknetwork.de>
2016-09-20 03:47:22 +0200

    Test task

---
+++ test/task.spec.js
@@ -0,0 +1,81 @@
+const {assert} = require('chai')
+const Shipit = require('shipit-cli')
+const Task = require('../task')
+
+describe('Task', function() {
+  describe('with global prefix, vendor and name', function() {
+    beforeEach(function() {
+      this.shipit = new Shipit({environment: 'test'})
+      this.shipit.initConfig({
+        default: {
+          docker: {
+            prefix: 'pre',
+            vendor: 'ven',
+            name: 'name'
+          }
+        },
+        test: {}
+      })
+    })
+
+    it('accepts task options', function() {
+      let task = new Task(this.shipit, {net: true})
+      assert.isOk(task.options.net)
+    })
+
+    it('merges task and global options', function() {
+      let task = new Task(this.shipit, {envs: ['AWESOME=TRUE']})
+      assert.equal(task.options.prefix, 'pre')
+      assert.equal(task.options.envs[0], 'AWESOME=TRUE')
+    })
+
+    it('overrides global options', function() {
+      let task = new Task(this.shipit, {prefix: 'pfix'})
+      assert.equal(task.options.prefix, 'pfix')
+    })
+
+    it('derives required names', function() {
+      let task = new Task(this.shipit)
+      assert.equal(task.options.image, 'pre/ven_name_web')
+      assert.equal(task.options.container, 'ven_name_web')
+    })
+
+    it('derives required names from options', function() {
+      let task = new Task(this.shipit, {
+        prefix: 'pfix',
+        vendor: 'vdor',
+        name: 'some'
+      })
+      assert.equal(task.options.image, 'pfix/vdor_some_web')
+      assert.equal(task.options.container, 'vdor_some_web')
+    })
+
+    it('uses required names from options', function() {
+      let task = new Task(this.shipit, {
+        image: 'pfix/vdor_some_web',
+        container: 'vdor_some_web'
+      })
+      assert.equal(task.options.image, 'pfix/vdor_some_web')
+      assert.equal(task.options.container, 'vdor_some_web')
+    })
+  })
+
+  describe('without global options', function() {
+    it('requires prefix without image and container', function() {
+      let shipit = new Shipit({environment: 'test'})
+      assert.throws(function() {
+        new Task(shipit, {}, /Missing prefix/)
+      })
+    })
+
+    it('does not require prefix with image and container', function() {
+      let shipit = new Shipit({environment: 'test'})
+      assert.doesNotThrow(function() {
+        new Task(shipit, {
+          image: 'pre/ven_name_web',
+          container: 'ven_name_web'
+        })
+      })
+    })
+  })
+})
